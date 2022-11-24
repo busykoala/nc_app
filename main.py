132781a79be83dbd7353c7d3e2b5131199d1811b
Matthias Osswald <matthias@busykoala.io>
2022-11-24 13:16:07 +0100

    return plain text instead of json.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 import socket
 
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 
 
 app = FastAPI()
@@ -20,43 +21,44 @@ def resolve_ip_host(host, port):
     return out
 
 
-def resolve_dns(url):
+def resolve_dns(host):
     try:
-        out = {"ip": socket.gethostbyname(url)}
+        out = f"ip: {socket.gethostbyname(host)}"
     except socket.gaierror:
-        out = {"ip": "not resolvable"}
+        out = "ip: not resolvable"
     return out
 
 
-@app.get("/")
+@app.get("/", response_class=PlainTextResponse)
 def root():
-    response = {
-        "/url": "with get params url/port",
-        "/ip": "with get params ip/port",
-    }
+    response = """
+Connectivity Checks:\n
+host route: with get params host & port (e.g. /host?host=example.com&port=443)\n
+ip route: with get params ip & port (e.g. /ip?ip=127.0.0.1&port=443)
+    """
     return response
 
 
-@app.get("/url")
-def url(url: Optional[str] = "", port: Optional[int] = 443):
-    dns = resolve_dns(url)
+@app.get("/host", response_class=PlainTextResponse)
+def host(host: Optional[str] = "", port: Optional[int] = 443):
+    dns = resolve_dns(host)
     connectivity = "unable to check"
-    if dns["ip"] != "not resolvable":
-        connectivity = resolve_ip_host(dns["ip"], port)
-    response = {
-        "url": url,
-        "port": port,
-        "dns": dns["ip"],
-        "connectivity": connectivity,
-    }
+    if dns.lstrip("ip: ") != "not resolvable":
+        connectivity = resolve_ip_host(dns.lstrip("ip: "), port)
+    response = """
+host: {}\n
+port: {}\n
+{}\n,
+connectivity: {}
+    """.format(host, port, dns, connectivity)
     return response
 
 
-@app.get("/ip")
+@app.get("/ip", response_class=PlainTextResponse)
 def ip(ip: Optional[str] = "", port: Optional[int] = 443):
-    response = {
-        "ip": ip,
-        "port": port,
-        "connectivity": resolve_ip_host(ip, port),
-    }
+    response = """
+host: {}\n
+port: {}\n
+connectivity: {}
+    """.format(ip, port, resolve_ip_host(ip, port))
     return response
